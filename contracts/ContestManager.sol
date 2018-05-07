3a9b72eb16d8c3dbd30a0ef67345060d725675a0
beeva-fernandocerezal <fernando.cerezal@beeva.com>
2018-05-07 12:43:58 +0200

    Added manager to contests relation

--- contracts/ContestManager.sol
+++ contracts/ContestManager.sol
@@ -98,46 +98,4 @@ contract ContestManager is ERC20Interface {
     event CreatedContest(address contest);
 }
 
-contract Contest {
-    uint registerFinalDate;
-    uint contestDate;
-    string description;
-    uint totalTickets;
-    uint availableTickets;
-    mapping(bytes32 => bool) public claimedTickets;
-
-    constructor( uint _registerFinalDate, uint _contestDate,string _description, uint _totalTickets) public {
-        registerFinalDate=_registerFinalDate;
-        description=_description;
-        contestDate=_contestDate;
-        totalTickets=_totalTickets;
-
-    } 
-    // not working!
-    function claimTicket(bytes32 code) public returns(bytes32) {
-        require(claimedTickets[code] == false);
-        require(availableTickets > 0);
-        availableTickets--;
-        if(availableTickets == 0){
-            emit NoMoreTickets();
-        }
-        claimedTickets[code] = true;
-        emit TicketClaimed(code);
-        return code;
-    }
-    function checkTicket(bytes32 code) public view returns (bool) {
-        return claimedTickets[code];
-    }
-    function freeTicket(bytes32 code) public returns (bytes32) {
-        require(claimedTickets[code] == true);
-        claimedTickets[code] = false;
-        emit TicketFreed(code);
-        availableTickets++;
-        claimedTickets[code] = false;
-        return code;
-    }
-    event TicketClaimed(bytes32 code);
-    event TicketFreed(bytes32 code);
-    event NoMoreTickets();
-}
 
