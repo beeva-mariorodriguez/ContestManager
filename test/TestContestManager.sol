ea5107f7fdb27e0103300bcd755b2dea004d433f
beeva-mariorodriguez <mario.rodriguez@beeva.com>
2018-05-10 16:24:41 +0200

    tests

--- test/TestContestManager.sol
+++ test/TestContestManager.sol
@@ -4,14 +4,26 @@ import "truffle/DeployedAddresses.sol";
 import "../contracts/ContestManager.sol";
 import "../contracts/Contest.sol";
 
-contract TestContestManager {
-    ContestManager cm = ContestManager(DeployedAddresses.ContestManager());
-    Contest contest = new Contest(1525937318, 1525937319, "test", 100, address(cm), 2);
-    function testBalanceOf0() public {
+contract TestContestManager 
+{
+    ContestManager cm = new ContestManager();
+    Contest contest;
+    function testBalanceOf0() public
+    {
         uint b = cm.balanceOf(0);
         Assert.equal(b, 0, "testBalanceOf() failed, balance should be 0");
     }
-    function testContestDeployment() public {
-        Assert.equal(address(cm), address(contest.cm()), "testContestDeployment() failed, contest->cm should be cm");
+
+    function testAddTokens() public
+    {
+        cm.addTokens(10,0);
+        Assert.equal(10, cm.balanceOf(0), "testAddTokens() failed, balance should be 10");
+    }
+    
+    function testNewContest() public
+    {
+        contest = Contest(cm.newContest(1525937318, 1525937319, "test",100, 2));
+        Assert.equal(address(cm), address(contest.cm()), "testNewContest() failed, bad contest->cm");
+        Assert.equal(cm.contests(contest), true, "testNewContest() failed, ContestManager->contests[] array not updated");
     }
 }
