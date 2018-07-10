7858ef0b9637ff7e1d2cd7fb98012a8163041d88
beeva-mariorodriguez <mario.rodriguez@beeva.com>
2018-07-10 18:32:33 +0200

    1 more js test

--- test/contestmanager.js
+++ test/contestmanager.js
@@ -1,8 +1,14 @@
 const cm = artifacts.require("ContestManager");
 
 contract('ContestManager JS test', async (accounts) => {
-    it("blablabla",async() =>{
+    it("deploy contract",async() =>{
         let instance = await cm.deployed();
+        let balance = await instance.balanceOf.call(0);
+        assert.equal(balance.valueOf(), 0);
+    });
+    it("add tokens", async() => {
+        let instance = await cm.deployed();
+        instance.addTokens.call(10,0);
         let balance = await instance.balanceOf.call(0);
         assert.equal(balance.valueOf(), 0);
     });
