bc8c108678bcccf95c85e3bb8ee82420d5da4155
beeva-mariorodriguez <mario.rodriguez@beeva.com>
2018-07-20 14:01:41 +0200

    test for ContestManager.newContest
    * not working!
    * where is my event?

--- test/contestmanager.js
+++ test/contestmanager.js
@@ -1,16 +1,43 @@
-const cm = artifacts.require("ContestManager");
+const contestmanager = artifacts.require("ContestManager");
+const contest = artifacts.require("Contest");
 
 contract('ContestManager JS test', async (accounts) => {
-    it("deploy contract",async() =>{
-        let instance = await cm.deployed();
-        let balance = await instance.balanceOf.call(0);
+    it("deploy contract",async () =>{
+        let cm = await contestmanager.deployed();
+        let balance = await cm.balanceOf.call(0);
         assert.equal(balance.valueOf(), 0);
     });
-    it("add tokens", async() => {
-        let instance = await cm.deployed();
-        instance.addTokens.call(10,0);
-        let balance = await instance.balanceOf.call(0);
+    it("add tokens", async () => {
+        let cm = await contestmanager.deployed();
+        cm.addTokens.call(10,0);
+        let balance = await cm.balanceOf.call(0);
         assert.equal(balance.valueOf(), 0);
+    });
+})
+
+contract('Deploy new Contest', async (accounts) => {
+    it("deploy contest", async () => {
+        let cm = await contestmanager.deployed();
+
+        let event = cm.CreatedContest((error, result) => {
+            if (!error)
+                console.log(result);
+            else
+                console.log(error);
+            this.CreatedContest.stopWatching();
+        });
+
+
+        let c = await cm.newContest.call(
+            Date.now() + 86400000,
+            Date.now() + 86401000,
+            "testContest",
+            100, 2);
+        event.stopWatching();
+        // c is the new contract's address
+        // console.log(c);
+        // assert.equal(true,
+        //     await cm.contests.call(c));
     });
 })
 
