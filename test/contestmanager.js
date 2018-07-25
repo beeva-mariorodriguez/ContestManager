70cff944b14924b5537efea2bca16be5f00e8cb3
beeva-mariorodriguez <mario.rodriguez@beeva.com>
2018-07-25 13:27:14 +0200

    js tests

--- test/contestmanager.js
+++ test/contestmanager.js
@@ -1,39 +1,45 @@
-const contestmanager = artifacts.require("ContestManager");
-const contest = artifacts.require("Contest");
+const ContestManager = artifacts.require("ContestManager");
+const Contest = artifacts.require("Contest");
 
-web3.eth.defaultAccount = '0x627306090abab3a6e1400e9345bc60c78a8bef57';
-
-contract('ContestManager JS test', async (accounts) => {
-    it("deploy contract",async () =>{
-        let cm = await contestmanager.deployed();
+contract('ContestManager: ', async (accounts) => {
+    it("initial balance of 0 should be 0",async () =>{
+        let cm = await ContestManager.deployed();
         let balance = await cm.balanceOf.call(0);
         assert.equal(balance.valueOf(), 0);
     });
-    it("add tokens", async () => {
-        let cm = await contestmanager.deployed();
+    it("should add tokens correctly", async () => {
+        let cm = await ContestManager.deployed();
         cm.addTokens.call(10,0);
         let balance = await cm.balanceOf.call(0);
         assert.equal(balance.valueOf(), 0);
     });
 })
 
-contract('Deploy new Contest', async (accounts) => {
-    it("deploy contest", async () => {
-        let cm = await contestmanager.deployed();
-        let event = cm.CreatedContest((error, result) => {
-            if (!error)
-                console.log(result);
-            else
-                console.log(error);
-            this.CreatedContest.stopWatching();
-        });
-
-
-        let c = await cm.newContest.call(
+contract('Contest: ', async (accounts) => {
+    let contest;
+    it("ContestManager should correcly deploy Contest", async () => {
+        let cm = await ContestManager.deployed();
+        let tx = await cm.newContest(
             Date.now() + 86400000,
             Date.now() + 86401000,
             "testContest",
             100, 2);
+        // tx.logs[0].args.contest <- get the newly deployed contest address
+        // from the event emitted by ContestManager.newContest()
+        // contest = Contest.at instantiate contract by address
+        contest = Contest.at(tx.logs[0].args.contest);
+        // will fail if the child contract was incorrectly instantiated
+        assert.equal(await contest.totalTickets(), 100);
+        // check if the child contract was correctly added to the mapping 
+        // ContestManager.contests[]
+        assert.equal(await cm.contests(contest.address), true);
     });
+    it("user should be able to claim a ticket", async () => {
+        let cm = await ContestManager.deployed();
+        cm.addTokens(10,web3.eth.accounts[0]);
+        let claimed = await contest.claimTicket.call()
+        assert.equal(claimed, true);
+    });
+
 })
 
--- truffle.js
+++ truffle.js
@@ -3,8 +3,7 @@ module.exports = {
         development: {
             host: "127.0.0.1",
             port: 9545,
-            network_id: "*",
-            from: "0x627306090abab3a6e1400e9345bc60c78a8bef57"
+            network_id: "*"
         }
     }
 };
